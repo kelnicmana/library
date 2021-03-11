7b1554fef5a195f0209490635df4418df23a8cad
Kellie Manasco <kellie.manasco@gmail.com>
2021-03-11 10:32:26 -0500

    Adds border around UI and scaling animation to popup. Read status now updates the objects' values in local storage upon toggling their associated button

--- script.js
+++ script.js
@@ -1,50 +1,147 @@
-let myLibrary = [];
-let thisTitle;
-let thisAuthor;
-let thisPages;
-let index;
-
-function Book(title, author, pages, status) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = status;
-    myLibrary.push(this);
-
-    this.element = document
-    console.log(myLibrary);
-}
-
-
-Book.prototype.status = function() {
-    if (document.querySelector('.status').value === "1") {
-        return `<button class="btn btn-success col-12" id="status "data-key="${index}" type="button">Read</button>`
-    }
-    else if (document.querySelector('.status').value === "0")  {
-        return `<button class="btn btn-outline-secondary col-12" id="status" data-key="${index}" type="button">Unread</button>`
+class Book {
+    constructor(_title, _author, _status) {
+        this.title = _title;
+        this.author = _author;
+        this.status= _status;
     }
 }
+class UI {
+    static displayBooks() {
+        
+        const books = Store.getBooks();
+        books.forEach((book) => UI.addBookToList(book));
+    }
+    static addBookToList(book) {
+        let statusButton;
+        if (book.status === "0") {
+            statusButton = `<div class="btn btn-success btn-sm status py-0 px-2">Read</div>`
+        }
+        else if (book.status === "1") {
+            statusButton = `<div class="btn btn-outline-secondary btn-sm status py-0 px-2 line">Read</div>`
+        }
+        const list = document.querySelector('.tbody');
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${statusButton}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete py-0 px-2">X</a></td>
+        `;
+        list.appendChild(row);
+    }
+    static toggleStatus(element) {
+        if(element.classList.contains('btn-success')) {
+            element.classList.remove('btn-success');
+            element.classList.add('btn-outline-secondary', 'line');
+        }
+        else if(element.classList.contains('btn-outline-secondary')) {
+            element.classList.remove('btn-outline-secondary', 'line');
+            element.classList.add('btn-success');
+        }
+        Store.toggleStatus(element.parentElement.previousElementSibling.previousElementSibling.textContent);
+    }
+    static deleteBook(element) {
+        if(element.classList.contains('delete')) {
+            element.parentElement.parentElement.remove();
+            
+            UI.showAlert('Book Deleted', 'dark');
+            Store.removeBook(element.parentElement.previousElementSibling.previousElementSibling.previousElementSibling.textContent);
+        }
+    }
+    static showAlert(text, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(text));
+        if (className === "danger") {
+            const cardBody = document.querySelector(".card-body");
+            const bookForm = document.querySelector("#book-form");
+            cardBody.insertBefore(div, bookForm);
+        }
+        else {
+            const container = document.querySelector('#table-container');
+            const table = document.querySelector('#table');
+            container.insertBefore(div, table);
+        }
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+    static clearFields() {
+        document.querySelector('.title').value = '';
+        document.querySelector('.author').value = '';
+        document.querySelector('select').selectedIndex = 0;
+    }
+}
+class Store {
+    static getBooks() {
+        let books;
+        if(localStorage.getItem('books') === null) {
+            books = [];
+        }
+        else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
 
-Book.prototype.event = function(e) {
-    switch (e.type) {
-        case "click": this.click(e)
+        return books;
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books));
+    } 
+
+    static toggleStatus(title) {
+        const books = Store.getBooks();
+        books.forEach((book) => {
+            if(book.title === title) {
+                (book.status === '1') ? book.status = '0' : book.status = '1';
+            }
+        });
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+
+    static removeBook(title) {
+        const books = Store.getBooks();
+
+        books.forEach((book, index) => {
+            if(book.title === title) {
+            books.splice(index, 1);
+            }
+        });
+        localStorage.setItem('books', JSON.stringify(books));
     }
 }
 
-Book.prototype.click = function(e) {
-    if (this.element.classList.contains("btn-success")) {
-        this.element.classList.add("btn-outline-secondary");
-    }
-}
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
-// function toggleStatus() {
-//     if (e.target.classList.contains("btn-success")) {
-//         e.target.classList.remove("btn-success").add("btn-outline-secondary");
-//     }
-//     if (e.target.classList.contains("btn-outline-secondary")) {
-//         e.target.classlist.remove("btn-outline-secondary").add("btn-success");
-//     }
-// }
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const title = document.querySelector('.title').value;
+    const author = document.querySelector('.author').value;
+    const status = document.getElementById('status').value;
+    
+    if(title === '' || author === '' || status === '') {
+        UI.showAlert('Please fill in all fields', 'danger');
+    }
+    else {
+        const book = new Book(title, author, status);
+        UI.addBookToList(book);
+        Store.addBook(book);
+        UI.showAlert('Book Added', 'success');
+        UI.clearFields();
+        togglePopUp();
+    }
+});
+
+document.querySelector('.tbody').addEventListener('click', (e) => {
+    UI.toggleStatus(e.target);
+});
+
+document.querySelector('.tbody').addEventListener('click', (e) => {
+    UI.deleteBook(e.target);
+});
 
 function togglePopUp() {
     if (document.querySelector('#popUp').classList.contains("show")) {
@@ -53,28 +150,4 @@ function togglePopUp() {
     else {
         document.querySelector('#popUp').classList.add("show");
     }
-}
-
-
-function addToLibrary() {
-    /// add event listener to submit button
-
-    thisTitle = document.querySelector('.title').value;
-    thisAuthor = document.querySelector('.author').value;
-    thisPages = document.querySelector('.pages').value;
-    thisRead = document.querySelector('.status').value;
-    new Book(thisTitle, thisAuthor, thisPages, thisRead);
-    
-    index = myLibrary.length - 1;
-
-    const table = document.querySelector('.tbody');
-    const tr = document.createElement('tr');
-    tr.setAttribute('data', `data-key :"${index}"`);
-    tr.innerHTML = `<td>${myLibrary[index].title}</td><td>${myLibrary[index].author}</td><td>${myLibrary[index].pages}</td><td>${myLibrary[index].status()}</td>`;
-    table.appendChild(tr);
-
-    document.querySelector('.title').value = "";
-    document.querySelector('.author').value = "";
-    document.querySelector('.pages').value = "";
-    document.querySelector('#popUp').classList.remove("show");
 }
